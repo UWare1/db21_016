573f53dd63f4bffdb8d48e72f426336c07cf7636
nutnareexx <nutnaree.co@ku.th>
2021-09-29 23:17:19 +0700

    Update detail_crud.php

--- views/pages/detail_crud.php
+++ views/pages/detail_crud.php
@@ -1,6 +1,7 @@
 <table border= 1>
     <tr><td> Qid </td> <td> PDid </td> <td> update </td> <td> delete </td>
-    <?php foreach($detailList as $detail){
+    <?php 
+        foreach($detailList as $detail){
         echo "<tr><td> $detail->QID</td>
                 <td>$detail->Pd_id</td>
                 <td> update </td> <td> delete </td>";
