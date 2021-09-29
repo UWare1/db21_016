973f7c732b1331881ca99bb0247e2014f05783dc
nutnareexx <nutnaree.co@ku.th>
2021-09-29 22:02:32 +0700

    Create connection_connect.php

---
+++ connection_connect.php
@@ -0,0 +1,17 @@
+
+<?php
+$severname = "localhost";
+$username = "db21_016";
+$password = "db21_016";
+$dbname = "db21_016";
+
+$conn = new mysqli($servername,$username,$password) ;
+
+if($conn->connect_error){
+  die("Connection failed:".$conn->connect_error);
+}
+echo"Successfully connected to sever <br>";
+
+$conn->close();
+
+?>
