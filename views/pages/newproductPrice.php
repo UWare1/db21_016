69b168bdf8bcc3064d0d1d96538956ebf0be9618
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-09-30 02:16:19 +0700

    Create newproductPrice.php

---
+++ views/pages/newproductPrice.php
@@ -0,0 +1,12 @@
+<form method="get" action="">
+<label>รหัสสินค้า<iselect name="PDid">
+    <?php foreach($ProductPrice_list as $PPL){echo "<option value= $PPL->PDid>$PPL->name</option>";}?>
+    </select></label><br>
+<label>จำนวนขั้นต่ำ<input type="text" name="minQty"/></label><br>
+<label>จำนวนมากสุด<input type="text" name="Qty"/></label><br>
+<label>ราคา<input type="text" name="price"/></label><br>
+<label>สกรีนเพิ่มสีละ<input type="text" name="screenPC"/></label><br>
+<input type = "hidden" name="controller" value="ProductPrice"/>
+<button type="submit" name="action" value="index">Back</button>
+<button type="submit" name="action" value="addProductPrice">Save</button>
+</form>
