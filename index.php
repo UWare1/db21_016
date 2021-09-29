934114f42c845abc880239e7fa82406da32d15e2
nutnareexx <nutnaree.co@ku.th>
2021-09-29 22:02:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ else{
     <head></head>
     <body>
         <?php echo "controller = ".$controller.",action = ".$action;?>
-        <br>[<a href=""> Home </a>]<br>
+        <br>[<a href="?controller=pages&action=home"> Home </a>]<br>
         <?php require_once("routes.php");?>
 </body>
 </html>
