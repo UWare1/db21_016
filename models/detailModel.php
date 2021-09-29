c66be7cd5e6b125f68a95947df6b186d1df699ba
nutnareexx <nutnaree.co@ku.th>
2021-09-30 01:08:39 +0700

    Update detailModel.php

--- models/detailModel.php
+++ models/detailModel.php
@@ -21,11 +21,11 @@ class Details{
         $result = $conn -> query($sql);
         while($my_row = $result->fetch_assoc())
         {
-            $QID=$my_row[Qid];
-            $Pd_id =$my_row[PDid];
-            $Pd_c=$my_row[PDcolor];
-            $qty =$my_row[QDqty];
-            $numc=$my_row[numColor];
+            $QID = $my_row[Qid];
+            $Pd_id = $my_row[PDid];
+            $Pd_c = $my_row[PDcolor];
+            $qty = $my_row[QDqty];
+            $numc= $my_row[numColor];
             $detailtList[] = new Details($QID,$Pd_id,$Pd_c,$qty,$numc);
         }
         require("connection_close.php");
