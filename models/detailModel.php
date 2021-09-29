fac264748dbf8aea97b2b60fbd7af3b90d62c9d0
nutnareexx <nutnaree.co@ku.th>
2021-09-30 00:14:55 +0700

    Update detailModel.php

--- models/detailModel.php
+++ models/detailModel.php
@@ -1,10 +1,17 @@
 <?php 
 class Details{
-    public $id;
-    public $name;
-    public function __construct ($id,$name) {
-        $this->id = $id;
-        $this->name = $name;
+    public $QID;
+    public $Pd_id;
+    public $Pd_c;
+    public $qty;
+    public $numc;
+
+    public function __construct ($qid,$pid,$c,$q,$nc) {
+        $this->QID = $qid;
+        $this->Pd_id = $pid;
+        $this->Pd_c = $c;
+        $this->qty = $q;
+        $this->numc = $nc;
     }
     public static function getAll()
     {
@@ -14,9 +21,12 @@ class Details{
         $result = $conn -> query($sql);
         while($my_row == $result->fetch_assoc())
         {
-            $depID=$my_row[Qid];
-            $depName =$my_row[PDid];
-            $departmentList[] = new Details($depID,$depName);
+            $QID=$my_row[Qid];
+            $Pd_id =$my_row[PDid];
+            $Pd_c=$my_row[PDcolor];
+            $qty =$my_row[QDqty];
+            $numc=$my_row[numColor];
+            $departmentList[] = new Details($QID,$Pd_id,$Pd_c,$qty,$numc);
         }
         require("connection_close.php");
         return $departmentList;
