fce968e995ab0b81d222028734aec6a804d794d3
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-09-30 02:38:26 +0700

    Update newproductPrice.php

--- views/pages/newproductPrice.php
+++ views/pages/newproductPrice.php
@@ -1,6 +1,6 @@
 <form method="get" action="">
 <label>รหัสสินค้า<select name="PDid">
-    <?php foreach($ProductPrice_list as $ProductPrice){echo "<option value= $ProductPrice->PDid>$ProductPrice->name</option>";}?>
+    <?php foreach($ProductPrice_list as $ProductPrice){echo "<option value= $ProductPrice->PDid>$ProductPrice->PDid</option>";}?>
     </select></label><br>
 <label>จำนวนขั้นต่ำ<input type="text" name="minQty"/></label><br>
 <label>จำนวนมากสุด<input type="text" name="Qty"/></label><br>
