87d4ad8c56686f9dc218daac9d5cf1fe6ccb98ed
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-09-30 02:28:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ else{
         <?php echo "controller = ".$controller.",action = ".$action;?>
         <br>[<a href="?controller=pages&action=home"> Home </a>]<br>
         <br>[<a href="?controller=ProductPrice&action=index"> ProductPrice </a>]
-            new ProductPrice<a href=?controller=ProductPrice&action=newProductPrice> click </a><br>
         <br>[<a href="?controller=detail&action=index"> Detail </a>]<br>
         <?php require_once("routes.php");?>
 </body>
