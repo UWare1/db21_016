92cc40da15b98b4144d09f81d20b7201972f4476
nutnareexx <nutnaree.co@ku.th>
2021-09-29 23:05:03 +0700

    Create detail_crud.php

---
+++ views/pages/detail_crud.php
@@ -0,0 +1,9 @@
+<table border= 1>
+    <tr><td> Qid </td> <td> PDid </td> <td> update </td> <td> delete </td>
+    <?php foreach($detailList as $detail){
+        echo "<tr><td> $detail->QID</td>
+                <td>$detail->Pd_id</td>
+                <td> update </td> <td> delete </td>";
+    }
+    echo "</table>";
+    ?>
