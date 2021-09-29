7f2f32654dc470ce9660d572a00acefab95415d2
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-09-30 02:58:17 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,5 +1,5 @@
 <?php 
-$controllers = array('pages'=>['home','error'],'detail'=>['index'], 'ProductPrice'=>['index','newProductPrice']);
+$controllers = array('pages'=>['home','error'],'detail'=>['index'], 'ProductPrice'=>['index','newProductPrice','addProductPrice']);
 
 function call($controller,$action){
     require_once("controllers/".$controller."_controller.php");
