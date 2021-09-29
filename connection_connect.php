a857995a90cd53091fe10426eb8549abea7ed6f2
nutnareexx <nutnaree.co@ku.th>
2021-09-30 00:40:00 +0700

    Update connection_connect.php

--- connection_connect.php
+++ connection_connect.php
@@ -7,8 +7,14 @@ $dbname = "db21_016";
 
 $conn = new mysqli($servername,$username,$password) ;
 
-if($conn->connect_error){
+if($conn->connect_error)
+{
   die("Connection failed:".$conn->connect_error);
+}
+
+if(!$conn->select_db($dbname))
+{
+  die("Connection failed:" .$conn->connect_error);
 }
 
 ?>
