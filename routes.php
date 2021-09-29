762f490d2356a6fc8543fd0b6743746cff4355cd
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-09-30 00:03:04 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,5 +1,8 @@
 <?php 
-$controllers = array('pages'=>['home','error'], 'ProductPrice'=>['index']);
+<<<<<<< HEAD
+=======
+$controllers = array('pages'=>['home','error'],'detail'=>['index'], 'ProductPrice'=>['index']);
+>>>>>>> 573f53dd63f4bffdb8d48e72f426336c07cf7636
 
 function call($controller,$action){
     require_once("controllers/".$controller."_controller.php");
@@ -7,9 +10,15 @@ function call($controller,$action){
     {
         case"pages": $controller = new PagesController();
         break;
+<<<<<<< HEAD
         case"ProductPrice": require_once("models/ProductPrice.php");
                             $controller = new ProductController();
                             break;
+=======
+        case"detail": require_once("models/detailModel.php");
+                        $controller = new detailController();
+        break;
+>>>>>>> 573f53dd63f4bffdb8d48e72f426336c07cf7636
     }
     $controller->{$action}();
 }
