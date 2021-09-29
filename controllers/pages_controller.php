ab44443bb515b692c7d747783e1245e2f490749d
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-09-30 02:33:47 +0700

    Update pages_controller.php

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -20,10 +20,6 @@ class PagesController
         require_once('views/pages/productPrice_crud.php');
     }
 
-    public function newproductPrice()
-    {
-        $productList = ProductPrice::getAll();
-        require_once('views/pages/newproductPrice.php');
-    }
+    
 }
 ?>
