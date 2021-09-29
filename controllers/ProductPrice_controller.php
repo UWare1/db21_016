13caa5bd935691180ed07ff2dcc7acbe1ed29efd
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-09-30 02:58:05 +0700

    Update ProductPrice_controller.php

--- controllers/ProductPrice_controller.php
+++ controllers/ProductPrice_controller.php
@@ -10,5 +10,16 @@ class ProductController {
         $productList = ProductPrice::getAll();
         require_once('views/pages/newproductPrice.php');
     }
+
+    public function addProductPrice() {
+        $PDid = $_GET['PDid'];
+        $minQty = $_GET['minQty'];
+        $Qty = $_GET['Qty'];
+        $price = $_GET['price'];
+        $screenPC = $_GET['screenPC'];
+        ProductPrice::Add($PDid,$minQty,$Qty,$price,$screenPC);
+
+        ProductController::index();
+    }
 }
 ?>
