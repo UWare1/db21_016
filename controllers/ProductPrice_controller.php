62eb5243b84d17ba40d3a87e83273b4610f0be78
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-09-30 00:52:55 +0700

    Update ProductPrice_controller.php

--- controllers/ProductPrice_controller.php
+++ controllers/ProductPrice_controller.php
@@ -1,7 +1,7 @@
 <?php class ProductController {
     public function index() {
         $product_list = ProductPrice::getAll();
-        require_once('views/ProductPrice/productPrice_crud.php');
+        require_once('views/pages/productPrice_crud.php');
     }
 
 }
