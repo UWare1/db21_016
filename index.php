cd4f299d2f71a86e2b8e53a7516bc104b1388b21
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-09-30 23:59:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ else{
     <body>
         <?php echo "controller = ".$controller.",action = ".$action;?>
         <br>[<a href="?controller=pages&action=home"> Home </a>]<br>
-        <br>[<a href="?controller=productprice&action=index"> ProductPrice </a>]
+        <br>[<a href="?controller=ProductPrice&action=index"> ProductPrice </a>]
             new ProductPrice<a href="?controller=ProductPrice&action=newProductPrice"> click </a><br>
             <form method="get" action="">
                 <input type="text" name="key">
@@ -25,10 +25,10 @@ else{
                 Search</button>
             </form>
             <td>
-                <a href="?controller=productprice&action=updateForm&PDid=$ProductPrice->PDid"> update </a>
+                <a href="?controller=ProductPrice&action=updateForm&PDid=$ProductPrice->PDid"> update </a>
             </td>
             <td>
-                <a href="?controller=productprice&action=deleteConfirm&PDid=$ProductPrice->PDid"> delete </a>
+                <a href="?controller=ProductPrice&action=deleteConfirm&PDid=$ProductPrice->PDid"> delete </a>
             </td>
         <br>[<a href="?controller=detail&action=index"> Detail </a>]<br>
         <?php require_once("routes.php");?>
