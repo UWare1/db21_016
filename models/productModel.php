eca8008faec1a0b588df5797290642d97c784b32
nutnareexx <nutnaree.co@ku.th>
2021-10-02 01:07:55 +0700

    Create productModel.php

---
+++ models/productModel.php
@@ -0,0 +1,33 @@
+<?php
+class Product{
+    public $pdid;
+    public $pdname;
+    public $pdtype;
+    public $pddes;
+
+    public function __construct ($id,$name,$type,$des) {
+        $this->pdid=$id;
+        $this->pdname=$name;
+        $this->pdtype=$type;
+        $this->pddes=$des;
+    }
+
+    public static function getAll()
+    {
+        $productList = [];
+        require("connection_connect.php");
+        $sql = "SELECT * FROM Product";
+        $result = $conn -> query($sql);
+        while($my_row = $result->fetch_assoc())
+        {
+            $pdid = $my_row[PDid];
+            $pdname = $my_row[PDname];
+            $pdtype = $my_row[PDtype];
+            $pddes = $my_row[PDdes];
+            $productList[] = new Product($pdid,$pdname,$pdtype,$pddes);
+        }
+        require("connection_close.php");
+        return $productList;
+    }
+}
+?>
