a002d29e91d7ba66b46f2a7c7f23fe89c629fcc2
nutnareexx <nutnaree.co@ku.th>
2021-10-02 04:21:40 +0700

    Update detail_new.php

--- views/details/detail_new.php
+++ views/details/detail_new.php
@@ -2,12 +2,12 @@ Hi
 <form method="get" action="">
 
     <label> Quotation ID <select name="Qid">
-        <?php foreach($quotationList as $quo) {echo "<option value=$quo->QID> $quo->QID </option>";}?>
+        <?php foreach($quotationList as $quo) {echo "<option value=$quo->Q_ID> $quo->Q_ID </option>";}?>
         </select>
     </label>
     <br>
     <label> Product ID <select name="PDid">
-        <?php foreach($productList as $pro) {echo "<option value=$pro->pdid> $quo->pdid </option>";}?>
+        <?php foreach($productList as $pro) {echo "<option value=$pro->pdid> $pro->pdid </option>";}?>
         </select>
     </label>
     <br>
