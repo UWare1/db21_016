005b52482a934b0ca32f698594cd5f8cf29ffbc3
nutnareexx <nutnaree.co@ku.th>
2021-10-02 03:05:27 +0700

    Delete datail_new.php

--- views/details/datail_new.php
+++
@@ -1,9 +0,0 @@
-<form method="get" action="">
-    <label> Quotation ID <select name="Qid">
-        <?php foreach($quotationList as $quo) {echo "<option value=$quo->QID> $quo->QID </option>";}?>
-        </select>
-    </label>
-    <label> Product ID <select name="PDid">
-        <?php foreach($ProductList as $pro) {echo "<option value=$pro->pdid> $quo->pdid </option>";}?>
-        </select>
-    </label>
