00b0bd6575903997c90f147a0356304810a9ae26
nutnareexx <nutnaree.co@ku.th>
2021-10-01 22:14:57 +0700

    Update detail_crud.php

--- views/details/detail_crud.php
+++ views/details/detail_crud.php
@@ -1,10 +1,12 @@
 <table border= 2>
-    <tr><td> Qid </td> <td> Product ID </td> <td> Product Color </td> <td> QDqty </td>
-    <td> numColor </td><td> update </td><td> Delete </td></tr>
+    <tr><td> Qid </td> <td> Product ID </td> <td> Product Name </td>  <td> Product Detail </td> 
+    <td> Product Color </td> <td> QDqty </td> <td> numColor </td><td> update </td><td> Delete </td></tr>
     <?php 
         foreach($detailList as $detail){
         echo "<tr><td> $detail->QID</td>
                 <td>$detail->Pd_id</td>
+                <td>$detail->Pd_name</td>
+                <td>$detail->Pd_d</td>
                 <td>$detail->Pd_c</td>
                 <td>$detail->qty</td>
                 <td>$detail->numc</td>
