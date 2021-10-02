40361e12c382cc4eb86398fceffca00057e53c1a
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-10-02 20:32:08 +0700

    Update newproductPrice.php

--- views/productprice/newproductPrice.php
+++ views/productprice/newproductPrice.php
@@ -1,13 +1,13 @@
 <form method="get" action="">
-<label>สินค้า<select name="PDid">
+<label>Product<select name="PDid">
     <option value=""> choose </option>
     <?php foreach($ProductList as $ProductPrice)
         {echo "<option value= $ProductPrice->PDid>$ProductPrice->PDname</option>";}?>
         </select></label><br>
-<label>จำนวนขั้นต่ำ<input type="text" name="minQty"/></label><br>
-<label>จำนวนมากสุด<input type="text" name="Qty"/></label><br>
-<label>ราคา<input type="text" name="price"/></label><br>
-<label>สกรีนเพิ่มสีละ<input type="text" name="screenPC"/></label><br>
+<label>minQty<input type="text" name="minQty"/></label><br>
+<label>Qty<input type="text" name="Qty"/></label><br>
+<label>Price<input type="text" name="price"/></label><br>
+<label>screenPC<input type="text" name="screenPC"/></label><br>
 <input type = "hidden" name="controller" value="ProductPrice"/>
 <button type="submit" name="action" value="index">Back</button>
 <button type="submit" name="action" value="addProductPrice">Save</button>
