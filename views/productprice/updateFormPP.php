ad888ae2e4e6a87f34074308f977ac36f7449daa
wtmllll <91516571+wtmllll@users.noreply.github.com>
2021-10-02 22:03:32 +0700

    Update updateFormPP.php

--- views/productprice/updateFormPP.php
+++ views/productprice/updateFormPP.php
@@ -1,21 +1,21 @@
 <form method="get" action="">
-    <label>รหัสสินค้า<select name="PDid">
+    <label>Product<select name="PDid">
         <option value=""> choose </option>
         <?php foreach($ProductList as $ProPrice)
             {echo "<option value= $ProPrice->PDid>";
-                if($ProPrice->PDid!=$ProductPrice->PDid) {echo "selected='selected'";}
+                if($ProPrice->PDid==$ProductPrice->PDid) {echo "selected='selected'";}
                 echo ">$ProPrice->name</option>";
             }?>
     </select></label><br>
-    <label>จำนวนขั้นต่ำ<input type="text" name="minQty"
+    <label>minQty<input type="text" name="minQty"
         value="<?php echo $ProductPrice->minQty;?>"/> </label><br>
-    <label>จำนวนมากสุด<input type="text" name="Qty"
+    <label>Qty<input type="text" name="Qty"
         value="<?php echo $ProductPrice->Qty;?>"/> </label><br>
-    <label>ราคา<input type="text" name="price"
+    <label>Price<input type="text" name="price"
         value="<?php echo $ProductPrice->price;?>"/> </label><br>
-    <label>สกรีนเพิ่มสีละ<input type="text" name="screenPC"
+    <label>screenPC<input type="text" name="screenPC"
         value="<?php echo $ProductPrice->screenPC;?>"/> </label><br>
-<input type="hidden" name="controller" value="productPrice"/>
+<input type="hidden" name="controller" value="ProductPrice"/>
 <button type="submit" name="action" value="index"> Back </button>
 <button type="submit" name="action" value="update"> update </button>
 </form>
